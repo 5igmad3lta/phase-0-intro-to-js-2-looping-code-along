9169ba9224f94e75d9a04813623ad673b764f20c
Stavros Droulias <stavros.d27@gmail.com>
2024-09-11 16:56:37 +1000

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+
+function writeCards(array, event) {
+    let messages = [];
+    for (let i = 0; i < array.length; i++){
+        const message = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+        console.log(messages);
+        debugger;
+    }
+    return messages;
+}
+writeCards(names, "surprise");
+
+function countDown(num) {
+    let countDown = num;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
+countDown(10);
